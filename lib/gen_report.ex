747ee2ef2ae2ebce44ab408866bd449df3678892
João Henrique <joao@mentat.solutions>
2021-10-10 15:28:08 -0300

    feat: Parallel processing of files

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -9,6 +9,49 @@ defmodule GenReport do
     {:error, "Insira o nome de um arquivo"}
   end
 
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Please provide a list of string!"}
+  end
+
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(report(), fn {:ok, result}, report -> sum_reports(report, result) end)
+  end
+
+  defp sum_reports(
+         %{
+           "all_hours" => all_hours1,
+           "hours_per_month" => hours_per_month1,
+           "hours_per_year" => hours_per_year1
+         },
+         %{
+           "all_hours" => all_hours2,
+           "hours_per_month" => hours_per_month2,
+           "hours_per_year" => hours_per_year2
+         }
+       ) do
+    all_hours = Map.merge(all_hours1, all_hours2, fn _key, value1, value2 -> value1 + value2 end)
+
+    hours_per_month = merge_report_fields(hours_per_month1, hours_per_month2)
+
+    hours_per_year = merge_report_fields(hours_per_year1, hours_per_year2)
+
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
+  end
+
+  defp merge_report_fields(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 ->
+      Map.merge(value1, value2, fn _key, final_value1, final_value2 ->
+        final_value1 + final_value2
+      end)
+    end)
+  end
+
   defp report, do: %{"all_hours" => %{}, "hours_per_month" => %{}, "hours_per_year" => %{}}
 
   defp sum_line(line, %{
